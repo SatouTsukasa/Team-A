4fa639f37e57dbca12f7f92f5c89d887c78a487f
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2018-12-05 09:34:03 +0900

    update

---
+++ ZI・MI・KO/Assets/Scripts/BulletManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletManager : MonoBehaviour {
+
+    public float Speed;
+    public float time = 0;
+
+	// Use this for initialization
+	void Start () {
+        Destroy(gameObject, 3.0f);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate()
+    {
+        transform.position -= transform.right * Time.deltaTime * Speed;
+    }
+}
---
+++ ZI・MI・KO/Assets/Scripts/CookieBlockBreak.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CookieBlockBreak : MonoBehaviour {
+
+    public LayerMask playerLayer;
+    private bool Break;
+    private Animator animator;
+
+    // Use this for initialization
+    void Start () {
+        animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Break = Physics2D.Linecast(transform.position, transform.position - transform.up * 1f, playerLayer);
+        //Debug.Log(Break);
+    }
+
+    private void OnCollisionEnter2D(Collision2D col)
+    {
+        Debug.Log("aaaa");
+        if (col.gameObject.tag == "Player")
+        {
+            if (Break)
+            {
+                animator.SetBool("BlockBreak", true);
+                Destroy(gameObject, 0.3f);
+            }
+        }
+    }
+}
