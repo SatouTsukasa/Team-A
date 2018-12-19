6fb1282af30214a69dea5dc249205aed8d6eafa4
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2018-12-19 10:37:40 +0900

    update

---
+++ ZI・MI・KO/Assets/Scripts/LifeManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LifeManager : MonoBehaviour {
+
+    public Sprite LifeHeart;
+    public float Life;
+    Sprite[] sprites; 
+    
+
+	// Use this for initialization
+	void Start () {
+        Life = 4;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //Debug.Log(Life);
+	}
+
+    public void damege()
+    {
+        GetComponent<Image>().fillAmount -= 0.125f;
+        Life -= 0.5f; 
+    }
+}
