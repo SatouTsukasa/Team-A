a452d6c1ce10b29d8cecb023a481edffaeffb283
SatouTsukasa <43591686+SatouTsukasa@users.noreply.github.com>
2019-01-24 09:40:06 +0900

    update2

---
+++ ZI・MI・KO/Assets/Scripts/StageSelectButton.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class StageSelectButton : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnClick(int number)
+    {
+        //Debug.Log(number);
+        switch (number)
+        {
+            case 0:
+                SceneManager.LoadScene("Main");
+                break;
+            case 1:
+                SceneManager.LoadScene("Main2");
+                break;
+            case 2:
+                SceneManager.LoadScene("StageSelect");
+                break;
+            case 3:
+                SceneManager.LoadScene("TitleScene");
+                break;
+            default:
+                break;
+        }
+    }
+}
